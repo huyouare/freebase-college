b68c768e36c5b913bc7200078283bb504b9e6775
Jesse Hu <huyouare@gmail.com>
2014-03-03 22:34:30 -0500

    Code refactoring, added permutations of state abbreviations, and confidence scoring, as well as true output format

--- README.md
+++ README.md
@@ -2,31 +2,31 @@ Upstart & Freebase College Reconciliation
 =========================================
 
 ###Goal:
-Write some code (an app, a script, etc.) that takes existing colleges and reconciles them to Google's Freebase.com database. Use whatever technologies and heuristics you can to make this as comprehensive and accurate as possible.
+Take existing colleges and reconcile them to Google's Freebase.com database. 
 
 ####Input:
-A spreadsheet of school information as a CSV or TSV. Format:
+A spreadsheet of school information as a TSV. Format:
 
 `Upstart ID,  Name,  Median SAT score,  Graduation rate, Retention rate`
 
 ####Output:
-Your code, along with its output as a CSV or TSV with the following format:
+Output as a TSV with the following format:
 
 `Upstart ID*, Freebase ID, Confidence %`
 
 This file should contain all of the schools we gave you in the initial file, and an English (not code) explanation of the methodology you used to generate the Confidence %. (*Upstart ID is just an arbitrary number we assign to schools, to give them a unique ID in our database.)
 
 ####Notes:
-* ".encode('utf-8')" necessary for strings with particular characters.
+* ".encode('utf-8')" necessary for strings with non-English characters.
 * Input file contains "--" when appending city or campus name to university name ("University of State--Campus"). The college name in the Freebase result usually appears as "University of State Campus", "University of State, Campus" or "University of State at Campus".
-* Changed state abbreviations to full names
+* Changed state abbreviations to full names. 
 http://code.activestate.com/recipes/577305-python-dictionary-of-us-states-and-territories/
-http://stackoverflow.com/questions/2313032/regex-for-state-abbreviations-python
 
 ###Procedure:
-* The Search API was used to find Freebase IDs by provided college names. Checking manually for results, this produced most of the data. The 'score' provided by Freebase Search and the difference in strings (difflib) determines whether or not to further reconcile
+* The Search API was used to find Freebase IDs by provided college names. Checking manually for results, this produced most of the data. The 'score' is provided by Freebase Search and 
+* The difference in strings (difflib) determines whether or not to further reconcile
 * Initially the Reconciliation endpoint of the Freebase API was considered for use to target those that could not be found via Search. However, results were not produced for all colleges provided.
-* Instead, variations of the college name were needed for Freebase to find the correct Freebase ID.
+* Instead, variations of the college name are needed for Freebase to find the correct Freebase ID.
 
 ####Corner Cases:
 * Rutgers, the State University of New Jersey--Newark:
@@ -38,8 +38,8 @@ Sometimes results appear with abbreviations rather than full state name (IN over
 Works with "Emmanuel College Georgia" but cannot include "Franklin Springs"
 * Union College - Barbourville, Kentucky and Union College - Schenectady, New York don't allow abbreviation "KY", though Union College - Lincoln, Nebraska does.
 * Very few abbreviated universities. These seem to be correct:
-JMU James Madison University
-Derby University of Derby
+JMU -> James Madison University
+Derby -> University of Derby
 
 
 ### Getting Started
@@ -53,7 +53,14 @@ Derby University of Derby
 `python upstart.py colleges_for_jesse.tsv`
 
 ###### Sample Output:  
-
+```
+upstart_id freebase_id confidence
+1 /m/02rk23 0.811214889875
+2 /m/01t8sr 0.841746194072
+3 /m/03818y 0.808804657761
+4 /m/02ldkf 0.808932328004
+5 /m/02hdht 0.7583775629631
+```
 
 ###### Testing (100 random trials):  
 `python test.py`
--- upstart.py
+++ upstart.py
@@ -3,14 +3,11 @@ import json
 import urllib
 import sys
 import re
-# import us
-# from bs4 import BeautifulSoup
+import math
 
-#TODO: Add state support without dragging confidence down
+states = { 'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AS': 'American Samoa', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District of Columbia', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'GU': 'Guam', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MP': 'Northern Mariana Islands', 'MS': 'Mississippi', 'MT': 'Montana', 'NA': 'National', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada', 'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'PR': 'Puerto Rico', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VA': 'Virginia', 'VI': 'Virgin Islands', 'VT': 'Vermont', 'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming' }
 
-states = { 'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AS': 'American Samoa', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District of Columbia', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'GU': 'Guam', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MP': 'Northern Mariana Islands', 'MS': 'Mississippi', 'MT': 'Montana', 'NA': 'National', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada', 'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'PR': 'Puerto Rico', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VA': 'Virginia', 'VI': 'Virgin Islands', 'VT': 'Vermont', 'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming'
-}
-
+collegeList = []
 
 class College(object):
   collegeCount = 0
@@ -31,24 +28,12 @@ def make_college(upstart_id, name, sat_score, graduatation_rate, retention_rate)
   college = College(upstart_id, name, sat_score, graduatation_rate, retention_rate)
   return college
 
-collegeList = []
-
 def read_file(f):
   with open(f, 'rb') as f:
     reader = csv.reader(f, delimiter='\t')
     next(reader)
     for row in reader:
-
-      # college_name = row[1].replace('--', ' ')
-      college_name = row[1]
-      regex = re.compile(r'\b(' + '|'.join(states) + r')\b')
-      abbr = regex.search(college_name)
-      if abbr:
-        abbr = abbr.group(0)
-        print(abbr)
-        college_name = college_name.replace(abbr, states[abbr])
-        print(college_name)
-      newCollege = College(row[0], college_name, (row[2] if len(row)>2 else None), (row[3] if len(row)>3 else None), (row[4] if len(row)>4 else None))
+      newCollege = College(row[0], row[1], (row[2] if len(row)>2 else None), (row[3] if len(row)>3 else None), (row[4] if len(row)>4 else None))
       collegeList.append(newCollege)
 
   print(College.collegeCount)
@@ -57,67 +42,134 @@ def process_reconcile(college):
   with open('reconcile.tsv', 'a') as rec:
     w = csv.writer(rec, delimiter = '\t')
     w.writerow([college.upstart_id] + [college.name] + [college.sat_score] + [college.retention_rate] + [college.graduatation_rate])
+  
+  #Try removing dashes and replacing abbreviation
+  college_name = college.name.replace('--', ' ')
+  regex = re.compile(r'\b(' + '|'.join(states) + r')\b')  # find all instances of state abbr.
+  abbr = regex.search(college_name)
+  if abbr:  # If abbreviation found, replace with full name
+    abbr = abbr.group(0)
+    print("Abbreviation: " + abbr)
+    college_name = college_name.replace(abbr, states[abbr])
+  print(college_name)
+
   params = {
-    'name': college.name,
+    'query': college_name,
+    'type': '/education/university', 
+    'key': api_key
+  }
+  rec_params = {
+    'name': college_name,
     'kind': '/education/university',
     # 'prop': ["/location/location/containedby:" + college.city, " /location/location/containedby:" + college.state],
     'key': api_key
   }
-  reconcile(college, params)
 
+  if abbr: # If name containes state abbreviation, search again
+    if(search(college, params)):
+      return True
+    if(reconcile(college, rec_params)):
+      return True
+    # Then try omitting abbreviations and double-dash entirely
+    college_name = college.name.replace('--', ' ')
+    regex = re.compile(r'\b(' + '|'.join(states) + r')\b')
+    abbr = regex.search(college_name)
+    abbr = abbr.group(0)
+    college_name = college_name.replace(abbr, ' ')
+    params['query'] = college_name
+    params['name'] = college_name
+    if(search(college, params)):
+      return True
+    if(reconcile(college, rec_params)):
+      return True
+
+  # Finally, we try just the university name without campus/city
+  college_name = college.name.split('--', 1)[0]
+  college_name = college_name.split('-', 1)[0]
+  params['query'] = college_name
+  rec_params['name'] = college.name
+  print(college_name)
+  if(search(college, params)):
+    print(college.result_name)
+    college.score = 10 # Mark confidence as very low
+    return True
+  if(reconcile(college, rec_params)):
+    print(college.result_name)
+    college.score = 10
+    return True
+
+  return False
+      
+def search(college, params):
+  service_url = 'https://www.googleapis.com/freebase/v1/search'
+  url = service_url + '?' + urllib.urlencode(params)
+  response = json.loads(urllib.urlopen(url).read())
+  if response['result']:
+    college.freebase_id = response['result'][0]['mid']
+    college.result_name = response['result'][0]['name'].encode('utf-8')
+    college.score = response['result'][0]['score']
+    return True
+  else:
+    return False
 
 
 def reconcile(college, params):
-
   service_url = 'https://www.googleapis.com/freebase/v1/reconcile'   
-
   url = service_url + '?' + urllib.urlencode(params)
   response = json.loads(urllib.urlopen(url).read())
-  with open('error.log', 'a') as f: # Simple logging, only for reconciliation
-    if response is None:
-      f.write("ERROR NO RESPONSE")
-      return
-    if 'match' in response.keys():
-      f.write("Match: " + response['match'])
-      college.freebase_id = response['match']['mid']
-      college.result_name = response['match']['name'].encode('utf-8')
-      college.confidence = response['match']['confidence']
-    elif 'candidate' in response.keys():
-      college.freebase_id = response['candidate'][0]['mid']
-      college.result_name = response['candidate'][0]['name'].encode('utf-8')
-      college.confidence = response['candidate'][0]['confidence']
-      for candidate in response['candidate']:
-        print(candidate['mid'] + ' (' + str(candidate['confidence']) + ')')
-    else:
-      college.result_name = None
-      # f.write("No candidates for: " + college.name)
+  if response is None:
+    return False
+  if 'match' in response.keys():
+    college.freebase_id = response['match']['mid']
+    college.result_name = response['match']['name'].encode('utf-8')
+    college.confidence = response['match']['confidence']
+    return True
+  if 'candidate' in response.keys():
+    college.freebase_id = response['candidate'][0]['mid']
+    college.result_name = response['candidate'][0]['name'].encode('utf-8')
+    college.confidence = response['candidate'][0]['confidence']
+    for candidate in response['candidate']:
+      print(candidate['mid'] + ' (' + str(candidate['confidence']) + ')')
+    return True
+  else:
+    college.result_name = None
+    return False
 
 def search_all():
   service_url = 'https://www.googleapis.com/freebase/v1/search'
-  with open('output.tsv', 'wb') as f:
+  with open('output-debug.tsv', 'wb') as f:
     writer = csv.writer(f, delimiter='\t')
     writer.writerow(["u_id"] + ["f_id"] + ["score"] + ["actual name"] + ["result name"])  
+    
     for college in collegeList:
       params = {
         'query': college.name,
         'type': '/education/university', 
         'key': api_key
       }
-
       url = service_url + '?' + urllib.urlencode(params)
       response = json.loads(urllib.urlopen(url).read())
 
       if len(response['result'])==0:
         print("Reconciling: " + college.name)
         process_reconcile(college)
+
       # elif response['result'][0]['score']<200:
       #   print("Score is: " + str(response['result'][0]['score']))
       #   process_reconcile(college)
+
       else:
         college.freebase_id = response['result'][0]['mid']
         college.result_name = response['result'][0]['name'].encode('utf-8')
         college.score = response['result'][0]['score']
+    
+      # if college.name != college.result_name:
+          # print(college.name + " VS \n" + college.result_name);
 
+      if(college.score<=1):
+        college.confidence = 0
+      else:
+        college.confidence = round(math.log(college.score, 4000), 4)
       if college.score:
         writer.writerow([college.upstart_id] + [college.freebase_id] + [college.score] + [college.name] + [college.result_name])
       elif college.confidence:
@@ -126,14 +178,16 @@ def search_all():
         writer.writerow([college.upstart_id] + [college.freebase_id] + [college.confidence] + [college.name] + ["No result"])
       
 def write_file():
-  return
-# if college.name != college.result_name:
-    # print(college.name + " VS \n" + college.result_name);
+  with open('output.tsv', 'wb') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerow(["upstart_id"] + ["freebase_id"] + ["confidence"])  
+    for college in collegeList:
+      writer.writerow([college.upstart_id] + [college.freebase_id] + [college.confidence])
 
 if __name__ == '__main__':
   api_key = open(".freebase_api_key").read()
-  # college = College(1, "Rutgers the State University of New Jersey Newark", 0, 0, 0)
-  # reconcile(college)
+  open('reconcile.tsv', 'w').close() # remove contents of file
+
   if len(sys.argv)==1: # if no arguments, use random Wikipedia page
     input_file = "colleges_for_jesse.tsv"
   else:
