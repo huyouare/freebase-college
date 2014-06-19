d52ee4748099cf550243ff271a163786f87b7a24
Jesse Hu <huyouare@gmail.com>
2014-06-19 13:12:50 -0700

    Append default numbers and rates

--- append-debt-and-default.py
+++ append-debt-and-default.py
@@ -34,12 +34,12 @@ with open('default_rates.csv', 'rU') as csvfile:
   next(reader, None)
   for row in reader:
     default_opeid.add(int(row[0] + '00'))
-    default_opeid_dict[int(row[0] + '00')] = row # 11 and 23
+    default_opeid_dict[int(row[0] + '00')] = row # 11 and 12
 
 with open('master-with-sat.csv', 'rU') as csvfile:
   reader = csv.reader(csvfile)
   row = next(reader)
-  writer.writerow( row[0:6] + ['Test Scores Required'] + row[6:] + ['Avg Debt of Graduates', 'NBD 1', 'NBR 1'] )
+  writer.writerow( row[0:6] + ['Test Scores Required'] + row[6:] + ['Avg Debt of Graduates', 'NBD 1', 'NBR 1', 'DRATE'] )
   for row in reader:
     new_row = row
     if (int(row[0]) in test_scores):
@@ -52,6 +52,10 @@ with open('master-with-sat.csv', 'rU') as csvfile:
       new_row = new_row + ['']
     if row[4] and (row[4] != '') and (row[4].isdigit()) and (int(row[4]) in default_opeid):
       new_row = new_row + [default_opeid_dict[int(row[4])][11], default_opeid_dict[int(row[4])][12]]
+      if (default_opeid_dict[int(row[4])][11] == '0.00' and default_opeid_dict[int(row[4])][12] == '0.00'):
+        a = 1
+      else:
+        new_row = new_row + [default_opeid_dict[int(row[4])][13]]
 
     writer.writerow(new_row)
 
